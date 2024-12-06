a485d8904bc9f8e1cb6d730d27952545763f79ec
Inder Kumar <inderik302@gmail.com>
2024-12-06 16:03:35 +0000

    Debug false

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-indk0-blogwalk20-3ly061r8pnx.ws.codeinstitute-ide.net','.herokuapp.com']
 
