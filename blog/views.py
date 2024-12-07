16bbbe63e307a3c2dd13fc95961b06bdc51ffe6b
Inder Kumar <inderik302@gmail.com>
2024-12-07 17:38:59 +0000

    Add code from URL and standard view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Event  
+
+class EventsList(generic.ListView):
+
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+
+
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
+    )
 
 # Create your views here.
 class PostList(generic.ListView):
