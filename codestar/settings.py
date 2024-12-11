3fa96a41b91e8a51bc495bfae348a6cc7bb207c5
Inder Kumar <inderik302@gmail.com>
2024-12-11 16:43:17 +0000

    Delete duplicated code

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['8000-indk0-blogwalk20-3ly061r8pnx.ws.codeinstitute-ide.net','.herokuapp.com']
 
